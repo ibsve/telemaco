5ffdaf4c03bde09f123ea6c8f0c88814ced8e0b3
ibsve <m.simonetti@ibs.ve.it>
2022-06-20 16:52:29 +0200

    aggiornata funzione random

--- src/TelemacoClient.php
+++ src/TelemacoClient.php
@@ -559,18 +559,16 @@ class TelemacoClient
      */
     public function random($length = 16)
     {
-        return (static::$randomStringFactory ?? function ($length) {
-            $string = '';
+        $string = '';
 
-            while (($len = strlen($string)) < $length) {
-                $size = $length - $len;
+        while (($len = strlen($string)) < $length) {
+            $size = $length - $len;
 
-                $bytes = random_bytes($size);
+            $bytes = random_bytes($size);
 
-                $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
-            }
+            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+        }
 
-            return $string;
-        })($length);
+        return $string;
     }
 }
